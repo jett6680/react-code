fadf947148b3a4e8b9f26f3c3722d9e9f05fd39c
powerjett <jindai.pjt@raycloud.com>
2023-03-28 19:26:20 +0800

    feat:change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import {  createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 import App from './App'
 
-const container = document.getElementById('root')
-createRoot(container).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+// const container = document.getElementById('root')
 
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
+
+// createRoot(container).render(
+    
+
+// )
 
 
